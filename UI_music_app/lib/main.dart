6a0530f356cadf22041afec5d09566be926f31fe
Huy28122003 <huy20031228@gmail.com>
2024-06-11 07:56:57 +0700

    tam

--- UI_music_app/lib/main.dart
+++ UI_music_app/lib/main.dart
@@ -1,9 +1,16 @@
-import './home.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:ui_music_app/screens/search.dart';
+import 'screens/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true,
+      ignoreSsl: true
+  );
   runApp(MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark), home: const Home()));
+      theme: ThemeData(brightness: Brightness.light), home: const Home()));
 }
 
 
